6b3952e78802defca79f9e42f6d2d4fbe38ef5bf
Miguel Ángel Navas García <manavas@gmail.com>
2025-02-12 15:46:28 +0900

    First component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Post from './components/Post';
+
+function App() {
+  return (
+    <main>
+      <Post author="Maximilian"  body="React rules"/>
+      <Post author="Miguel"  body="Potros"/>
+      </main>
+  )
+}
+
+export default App
