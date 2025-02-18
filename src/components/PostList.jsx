66b8eb408fc2358154b9897e9b4903db8c79c753
Miguel Ángel Navas García <manavas@gmail.com>
2025-02-18 10:33:35 +0900

    setup event listeners

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,16 @@
 import Post from './Post';
+import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 
 function PostList() {
     return (
-        <ul className={classes.posts}>
-            <Post author="Maximilian" body="React rules" />
-            <Post author="Miguel" body="me gusta protos" />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
+                <Post author="Maximilian" body="React rules" />
+                <Post author="Miguel" body="me gusta protos" />
+            </ul>
+        </>
     )
 }
 export default PostList;
