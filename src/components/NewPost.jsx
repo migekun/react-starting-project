aed2e16e9abb9ff763820a4539c2e9e5c3cbb464
Miguel Ángel Navas García <manavas@gmail.com>
2025-02-21 14:45:35 +0900

    añadida la modal

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,18 +1,19 @@
 import classes from './NewPost.module.css';
 
-function NewPost() {
-    function changeBodyHandler(event) {
-        console.log(event.target.value);
-    }
+function NewPost(props) {
+    /*const stateData = useState('');
+    stateData[0]; // current value
+    stateData[1]; // state function to update
+    */
     return (
         <form className={classes.form}>
         <p>
             <label htmlFor="body">Text</label>
-            <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+            <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
         </p>
         <p>
             <label htmlFor="name">Your name</label>
-            <input type="text" id="name" required />
+            <input type="text" id="name" required onChange={props.onAuthorChange}/>
         </p>
         </form>
     );
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,37 @@
+import { useState }  from 'react';
 import Post from './Post';
 import NewPost from './NewPost';
+import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostList() {
+    const [modalIsVisible, setModalIsVisible] = useState(true)
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function closeModalHandler() {
+        setModalIsVisible(false);
+    }
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
     return (
         <>
-            <NewPost />
+            {modalIsVisible && ( 
+                <Modal onClose={closeModalHandler}>
+                    <NewPost 
+                        onBodyChange={bodyChangeHandler} 
+                        onAuthorChange={authorChangeHandler}/>
+                </Modal>
+            )}
             <ul className={classes.posts}>
-                <Post author="Maximilian" body="React rules" />
+                <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Miguel" body="me gusta protos" />
             </ul>
         </>
